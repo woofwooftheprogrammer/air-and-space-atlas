ac85af072e9a5c14fea83f7fdf8dbf70c8b14642
woofwooftheprogrammer <36029297+woofwooftheprogrammer@users.noreply.github.com>
2024-05-04 12:23:34 +0200

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace air_object_movements_net_core.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
