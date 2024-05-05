3909cc45a16f9b6c0102e52732c2603f8d60e727
woofwooftheprogrammer <36029297+woofwooftheprogrammer@users.noreply.github.com>
2024-05-05 16:44:52 +0200

    Rename namespaces #19

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace air_object_movements_net_core.Pages;
+namespace air_and_space_atlas.Pages;
 
 public class PrivacyModel : PageModel
 {
